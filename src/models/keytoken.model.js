00fe4b889bc10adb838c8e9d3d84b8d963a43e09
PhanMinhSon <sonpm@hivelab.com>
2024-02-23 15:52:39 +0700

    feat: sign up with rsa

---
+++ src/models/keytoken.model.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose"); // Erase if already required
+
+// chứa keyPublicKey và refreshToken
+// Declare the Schema of the Mongo model
+
+const DOCUMENT_NAME = "Key";
+const COLLECTION_NAME = "Keys";
+
+var keyTokenSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Shop",
+    },
+    publicKey: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    collection: COLLECTION_NAME,
+    timestamps: true,
+  }
+);
+
+//Export the model
+module.exports = model(DOCUMENT_NAME, keyTokenSchema);
